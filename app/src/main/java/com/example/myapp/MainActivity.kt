2c6c70bbfd83ebbe7404811a0b5842e24820abb1
retroboy999 <123660865+MateusHv999@users.noreply.github.com>
2023-07-31 01:11:15 -0300

    Upload Projeto!
    Um app básico que utiliza extensão de funções, com foco em mudar imagens por click e mostrar nome do usuário/ clicks que foram feitos.

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
+
+class MainActivity : AppCompatActivity() {
+    var numClicks = 0
+    var textbox: EditText? = null
+    var userInfo: TextView? = null
+    var layout: LinearLayoutCompat? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        userInfo = findViewById<TextView>(R.id.userinfo)
+        textbox = findViewById<EditText>(R.id.textbox)
+        layout = findViewById<LinearLayoutCompat>(R.id.MainLayout)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            myStyle()
+            numClicks++
+            userInfo?.text = "Olá ${textbox?.text} você já clicou ${numClicks} vezes"
+        }
+    }
+
+    fun myStyle() {
+        when {
+            numClicks % 2 == 0 -> layout?.background = getDrawable(R.mipmap.fundo_1)
+            numClicks % 3 == 0 -> layout?.background = getDrawable(R.mipmap.fundo_2)
+            numClicks % 4 == 0 -> layout?.background = getDrawable(R.mipmap.fundo_3)
+            else -> layout?.background = null
+        }
+    }
+}
